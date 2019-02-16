a392e5e1676f462d09e34ff0f473484baec281a1
Ty Ackerman <tyler.raye.ackerman@gmail.com>
2019-02-16 02:50:43 -0500

    finished form, now just need to append it to the week

--- src/components/CreateExercise.jsx
+++ src/components/CreateExercise.jsx
@@ -1,10 +1,71 @@
 import React, { Component } from "react";
 
 class CreateExercise extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.formatTempo());
+  };
+
+  formatTempo = () => {
+    return `${this.refs.tempo1.value}-${this.refs.tempo2.value}-${
+      this.refs.tempo3.value
+    }-${this.refs.tempo4.value}`;
+  };
+
   render() {
     return (
       <div>
-        <p>CreateExercise</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Exercise:
+            <input type="text" name="exercise" ref="exercise" required />
+          </label>
+          <label>
+            Sets:
+            <input type="number" ref="sets" required />
+          </label>
+          <label>
+            Reps:
+            <input type="number" ref="reps" required />
+          </label>
+          <label>
+            Modifications:
+            <select ref="mods">
+              <option value="none">None</option>
+              <option value="mod1">Mod1</option>
+              <option value="mod2">Mod2</option>
+              <option value="mod3">Mod3</option>
+            </select>
+          </label>
+          <label>
+            Tempo:
+            <input
+              type="number"
+              ref="tempo1"
+              required
+              placeholder="Eccentric"
+            />
+            <input
+              type="number"
+              ref="tempo2"
+              required
+              placeholder="Pause (Midpoint)"
+            />
+            <input
+              type="number"
+              ref="tempo3"
+              required
+              placeholder="Concentric"
+            />
+            <input
+              type="number"
+              ref="tempo4"
+              required
+              placeholder="Pause (Top)"
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
